203b5b3f42dc8a388090314c5ccb113c4ea74fc7
magasteph <stephanne.victoria@gmail.com>
2024-07-29 12:52:23 -0300

    tarefas

---
+++ src/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package OperacoesBasicas;
+
+public class Tarefa {
+
+    //atributo
+
+    private String descricao;
+
+    public Tarefa (String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    
+
+}
