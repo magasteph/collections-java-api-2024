0035f2fa12bf76b060f4ebaf2f668132a31fd4e1
magasteph <stephanne.victoria@gmail.com>
2024-08-05 14:54:48 -0300

    list - ordenacao

---
+++ src/List/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,31 @@
+package List.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    // atributo
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordernarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordernarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura);
+        return pessoasPorAltura;
+    }
+}
